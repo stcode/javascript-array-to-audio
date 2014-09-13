0faac628bada4f694d0aaab9de67494a6d3db4d5
ljv5555 <jjverderber@gmail.com>
2014-09-13 18:44:07 -0500

    l

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 
 function runInWorker(cmd)
 {
-var w= new Worker("js/worker.js");
-w.onmessage = function(event){
-	var r = document.getElementById("result");
-	if(r){}else{r=document.createElement('div');r.setAttribute('id','result');document.appendChild(r);}
-    var dm = event || {};
-    dm=dm.data || {};
-    dm=dm.message || '';
-	r.innerHTML = ''+dm;
-};
-w.postMessage(" ( 'message returned: ' + ( 1 + 1.02 ) ) ");
+	var w= new Worker("js/worker.js");
+	w.onmessage = function(event){
+		var r = document.getElementById("result");
+		if(r){}else{r=document.createElement('div');r.setAttribute('id','result');document.appendChild(r);}
+	    var dm = event || {};
+	    dm=dm.data || {};
+	    dm=dm.message || '';
+		r.innerHTML = ''+dm;
+	};
+	w.postMessage(" ( 'message returned: ' + ( 1 + 1.02 ) ) ");
 }
