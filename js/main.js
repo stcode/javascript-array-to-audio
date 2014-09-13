8167133ccb74d94a538691f29bf94d519831b24d
ljv5555 <jjverderber@gmail.com>
2014-09-13 18:11:14 -0500

    .

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var w= new Worker("worker.js");
+w.onmessage = function(event){
+	var r = document.getElementById("result");
+	if(r){}else{r=document.createElement('div');r.setAttribute('id','result');document.appendChild(r);}
+    var dm = event || {};
+    dm=dm.data || {};
+    dm=dm.message || '';
+    
+	r.innerHTML = ''+dm;
+};
